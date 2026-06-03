d0d2e5df69b79b04cd4d56adf751d82446bc18cc
Ashaaardtp <ungu55.pr@gmail.com>
2026-06-03 14:57:24 +0700

    Update

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-# modelling.py
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
@@ -6,19 +5,17 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import mlflow
 import mlflow.sklearn
 import os
-import warnings
-warnings.filterwarnings("ignore")
 
 def main():
-    data_dir = "MLProject/california_housing_preprocessed"
+    data_dir = "california_housing_preprocessed"
     X_train = pd.read_csv(os.path.join(data_dir, "X_train_preprocessed.csv"))
     X_test = pd.read_csv(os.path.join(data_dir, "X_test_preprocessed.csv"))
     y_train = pd.read_csv(os.path.join(data_dir, "y_train.csv")).values.ravel()
     y_test = pd.read_csv(os.path.join(data_dir, "y_test.csv")).values.ravel()
-    
+
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
     mlflow.set_experiment("CI_Retraining")
-    
+
     with mlflow.start_run(run_name="CI_Run"):
         mlflow.sklearn.autolog()
         model = RandomForestRegressor(n_estimators=100, random_state=42)
